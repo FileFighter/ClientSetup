c3db38547575a08e7c0a301b495fcf83e2e59395
open-schnick <jonathan.burst@gmx.de>
2020-10-09 11:40:54 +0200

    finished artwork

--- init_setup.sh
+++ init_setup.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
-source config.shlib; # load the config library functions
-
-# Read in config keys.
-frontend_port="$(read ./config.cfg frontend_port)";
-rest_port="$(read ./config.cfg  rest_port)"
-db_port="$(read ./config.cfg  db_port)"
-db_name="$(read ./config.cfg db_name)"
-db_user="$(read ./config.cfg  db_user)"
-db_password="$(read ./config.cfg  db_password)"
-
-if [ $db_password = "" ]; then
-  # set it somehow.
-  db_password="asdasdasd"
-  write ./config.cfg db_password $db_password
-fi
+source lib/config.shlib; # load the config library functions
 
 # Check if docker is running
 if ! docker info >/dev/null 2>&1; then
@@ -21,8 +7,67 @@ if ! docker info >/dev/null 2>&1; then
     exit 1
 fi
 
-# Docker is running try pulling images.
-echo "Docker instance found."
+# Startup Message.
+echo ""
+echo "------------------------< FileFighter >------------------------"
+echo "->          Version 0.0.1 last updated at 09.10.20           <-"
+echo "->       Developed by Gimleux, Valentin, Open-Schnick.       <-"
+echo "->    Development Blog: https://www.filefighter.github.io    <-"
+echo "-> The code can be found at: https://github.com/filefighter  <-"
+echo "-----------------------< Initial Setup >-----------------------"
+echo ""
+echo "Docker prequesites matched. Docker instance running"
+echo "Reading in config file at: config.cfg"
+
+# Read in default keys.
+frontend_port="$(read ./lib/config.cfg.defaults frontend_port)";
+rest_port="$(read ./lib/config.cfg.defaults  rest_port)"
+db_port="$(read ./lib/config.cfg.defaults  db_port)"
+db_name="$(read ./lib/config.cfg.defaults db_name)"
+db_user="$(read ./lib/config.cfg.defaults  db_user)"
+db_password="$(read ./lib/config.cfg.defaults  db_password)"
+
+# Overwrite default with custom config.
+if [ has_key ./config.cfg $frontend_port ]; then
+  # set it
+  frontend_port="$(read ./config.cfg frontend_port)";
+fi
+
+if [ has_key ./config.cfg $rest_port ]; then
+  # set it
+  rest_port="$(read ./config.cfg rest_port)";
+fi
+
+if [ has_key ./config.cfg $db_port ]; then
+  # set it
+  db_port="$(read ./config.cfg db_port)";
+fi
+
+if [ has_key ./config.cfg $db_name ]; then
+  # set it
+  db_name="$(read ./config.cfg db_name)";
+fi
+
+if [ has_key ./config.cfg $db_user ]; then
+  # set it
+  db_user="$(read ./config.cfg db_user)";
+fi
+
+if [ has_key ./config.cfg $db_password ]; then
+  # set it
+  db_password="$(read ./config.cfg db_password)";
+fi
+
+# Check if password was empty
+if [ $db_password = "" ]; then
+  # Create new Password
+  db_password="asdasdasd"
+  write ./config.cfg db_password $db_password
+fi
+
+# Finished Config:
+echo "Finished reading config file "
+# CREATE CONTAINERS.
 
 # Database
 echo "Create DB Container."
