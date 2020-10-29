04658a6554d4d2379f2406ee0f09a6ead7e8cc4f
open-schnick <jonathan.burst@gmx.de>
2020-10-29 13:26:17 +0100

    made script more fancy.

--- init_setup.sh
+++ init_setup.sh
@@ -16,21 +16,28 @@ dbname="FileFighterDB"
 networkname="FileFighterNetwork"
 
 # latest stable versions.
-frontendVersion="latest" #"$(getTagsByName filefighter/frontend v | tail -1)"
-restVersion="latest" #$(getTagsByName filefighter/rest v | tail -1)"
+frontendVersion="latest"
+restVersion="latest"
 
 # Startup Message.
+
+echo "  _____   _   _          _____   _           _       _                 "
+echo " |  ___| (_) | |   ___  |  ___| (_)   __ _  | |__   | |_    ___   _ __ "
+echo " | |_    | | | |  / _ \ | |_    | |  / _\` | | '_ \  | __|  / _ \ | '__|"
+echo " |  _|   | | | | |  __/ |  _|   | | | (_| | | | | | | |_  |  __/ | |   "
+echo " |_|     |_| |_|  \___| |_|     |_|  \__, | |_| |_|  \__|  \___| |_|   "
+echo "                                     |___/                             "
+echo "                   Version 1.2 Last updated at 29.10.20                "
+echo "              Developed by Gimleux, Valentin, Open-Schnick.            "
+echo "            Development Blog: https://filefighter.github.io            "
+echo "       The code can be found at: https://www.github.com/filefighter    "
 echo ""
-echo "-------------------------< FileFighter >--------------------------"
-echo "|              Version 1.0 Last updated at 14.10.20              |"
-echo "|         Developed by Gimleux, Valentin, Open-Schnick.          |"
-echo "|       Development Blog: https://filefighter.github.io          |"
-echo "|  The code can be found at: https://www.github.com/filefighter  |"
-echo "--------------------< Started Initial Setup >---------------------"
+echo "-----------------------< Started First Setup >-------------------------"
 echo ""
 echo "Docker prequesites matched. Docker instance running."
 echo "Reading in config file from: $configFilePath."
 
+exit 0;
 # Read in default keys.
 frontend_port="$(read ./config.cfg frontend_port)";
 rest_port="$(read ./config.cfg  rest_port)"
