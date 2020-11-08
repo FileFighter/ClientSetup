f29141b44aa503b2df2fbcb005f92c189dc85340
open-schnick <jonathan.burst@gmx.de>
2020-11-08 13:03:44 +0100

    Added additional information for the user.

--- lib/remove.sh
+++ lib/remove.sh
@@ -4,9 +4,16 @@ ffremove() {
   frontendname="FileFighterFrontend"
   dbname="FileFighterDB"
 
-  docker container stop $restname && docker container rm $restname
-  docker container stop $frontendname && docker container rm $frontendname
-  docker container stop $dbname && docker container rm $dbname
+  if [[ $(docker ps -a --format "{{.Names}}" | grep $restname) ]] || [[ $(docker ps -a --format "{{.Names}}" | grep $frontendname) ]] || [[ $(docker ps -a --format "{{.Names}}" | grep $dbname) ]]; then
+    docker container stop $restname && docker container rm $restname
+    docker container stop $frontendname && docker container rm $frontendname
+    docker container stop $dbname && docker container rm $dbname
 
-  echo "Removed FileFighter Application. Install it again with 'ffighter install'."
+    echo "Removed FileFighter Application. Install it again with 'ffighter install'."
+    exit 0
+  fi
+
+  echo "FileFighter Application not found. Install it with 'ffighter install'."
+  echo ""
+  exit 1;
 }
--- lib/start.sh
+++ lib/start.sh
@@ -15,16 +15,23 @@ ffstart() {
   frontendname="FileFighterFrontend"
   dbname="FileFighterDB"
 
-  echo "Docker prerequisites matched. Docker instance running."
-  echo "Starting services..."
+  if [[ $(docker ps -a --format "{{.Names}}" | grep $restname) ]] || [[ $(docker ps -a --format "{{.Names}}" | grep $frontendname) ]] || [[ $(docker ps -a --format "{{.Names}}" | grep $dbname) ]]; then
+    echo "Docker prerequisites matched. Docker instance running."
+    echo "Starting services..."
 
-  docker start $restname
-  docker start $frontendname
-  docker start $dbname
+    docker start $restname
+    docker start $frontendname
+    docker start $dbname
 
+    echo ""
+    echo "Finished starting FileFighter services."
+    echo "Frontend is running here: http://localhost:$frontendport."
+    echo "You can stop them again with 'ffighter stop'."
+    echo ""
+    exit 0
+  fi
+
+  echo "FileFighter Application not found. Install it with 'ffighter install'."
   echo ""
-  echo "Finished starting FileFighter services."
-  echo "Frontend is running here: http://localhost:$frontendport."
-  echo "You can stop them again with 'ffighter stop'."
-  echo ""
+  exit 1;
 }
