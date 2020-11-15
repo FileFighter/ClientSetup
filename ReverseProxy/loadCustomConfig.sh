9650e2df2c90c0933406e5c87f4354151871a7ad
qvalentin <valentin.theodor@web.de>
2020-11-15 17:53:32 +0100

    do /sonarqube

--- ReverseProxy/loadCustomConfig.sh
+++ ReverseProxy/loadCustomConfig.sh
@@ -1,1 +1,6 @@
 docker cp /home/Bike2DH/ClientSetup/ReverseProxy/nginx.conf FileFighterReverseProxy:/etc/nginx/nginx.conf
+
+docker network connect FileFighterNetwork sonarqube_sonarqube_1
+
+docker stop FileFighterReverseProxy
+docker start FileFighterReverseProxy
