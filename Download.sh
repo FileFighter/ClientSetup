88fdfa08a9835ac5ca1ecd007aa492f9b515ef3f
open-schnick <jonathan.burst@gmx.de>
2021-05-14 12:17:42 +0200

    Fixed bug in download script

--- Download.sh
+++ Download.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
+DOWNLOAD_DIR="/tmp"
+
+cd $DOWNLOAD_DIR
 echo "Downloading FileFighter ClientSetup."
 curl https://codeload.github.com/FileFighter/ClientSetup/zip/master > FileFighter.zip
 
 echo ""
 echo "finshed downloading."
 
-if ! unzip >/dev/null 2>&1; then
+if ! unzip>/dev/null 2>&1; then
   echo "'unzip' not found. Install it with 'sudo apt install unzip'."
   exit 1
 fi
 
-
-if [ ! -d "FileFighter" ]; then
-    mkdir FileFighter
-fi
-
-unzip FileFighter.zip -d ./FileFighter
+unzip -o FileFighter.zip >/dev/null 2>&1
 
 echo ""
 echo "finished unpacking FileFighter. Installing it now..."
 echo ""
 
-chmod -R +x FileFighter/
-$(pwd)/FileFighter/ClientSetup-master/Install.sh
+chmod -R +x $DOWNLOAD_DIR/ClientSetup-master/
+cd $DOWNLOAD_DIR/ClientSetup-master/
+./Install.sh
